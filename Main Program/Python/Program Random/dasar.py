8321276e8ac5cd670d9528076e973f6027e5c688
sibeux <wahabinasrul@gmail.com>
2022-02-18 23:54:44 +0700

    palindrome

--- Main Program/Python/Program Random/dasar.py
+++ Main Program/Python/Program Random/dasar.py
@@ -16,3 +16,14 @@ print('sibe')
 #fungsi
 def fungsi(a,b):
     return a+b
+
+#palindrome
+def palindrome(kata):
+    if len(kata) <= 1:
+        return True
+    elif kata[0] != kata[-1]:
+        return False
+    else:
+        return palindrome(kata[1:-1])
+
+print(palindrome('katak'))
