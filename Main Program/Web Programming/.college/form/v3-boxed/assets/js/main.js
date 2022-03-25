68cc803608f378242efcd10835b7d1046aee6a1b
sibeux <wahabinasrul@gmail.com>
2022-03-26 00:23:06 +0700

    form

---
+++ Main Program/Web Programming/.college/form/v3-boxed/assets/js/main.js
@@ -0,0 +1,266 @@
+$(function() {
+
+    $('.wizard > .steps li a').click(function() {
+        $(this).parent().addClass('checked');
+        $(this).parent().prevAll().addClass('checked');
+        $(this).parent().nextAll().removeClass('checked');
+    });
+    // Custome Jquery Step Button
+    $('.forward').click(function() {
+        $("#wizard").steps('next');
+    })
+    $('.backward').click(function() {
+        $("#wizard").steps('previous');
+    })
+    // Select Dropdown
+    $('html').click(function() {
+        $('.select .dropdown').hide();
+    });
+    $('.select').click(function(event) {
+        event.stopPropagation();
+    });
+    $('.select .select-control').click(function() {
+        $(this).parent().next().toggle();
+    })
+    $('.select .dropdown li').click(function() {
+        $(this).parent().toggle();
+        var text = $(this).attr('rel');
+        $(this).parent().prev().find('div').text(text);
+    })
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    $(document).ready(function() {
+        $(".custom-file-input").on("change", function() {
+            var fileName = $(this).val().split("\\").pop();
+            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+        });
+
+        $('#slider-service').slick({
+            dots: true,
+            rows: 2,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    rows: 2,
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    rows: 1,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    rows: 1,
+                    dots: false,
+                    arrows: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            ]
+        });
+
+    });
+
+
+})
+
+
+
+
+//multi form ===================================
+//DOM elements
+const DOMstrings = {
+    stepsBtnClass: 'multisteps-form__progress-btn',
+    stepsBtns: document.querySelectorAll(`.multisteps-form__progress-btn`),
+    stepsBar: document.querySelector('.multisteps-form__progress'),
+    stepsForm: document.querySelector('.multisteps-form__form'),
+    stepFormPanelClass: 'multisteps-form__panel',
+    stepFormPanels: document.querySelectorAll('.multisteps-form__panel'),
+    stepPrevBtnClass: 'js-btn-prev',
+    stepNextBtnClass: 'js-btn-next'
+};
+
+
+//remove class from a set of items
+const removeClasses = (elemSet, className) => {
+
+    elemSet.forEach(elem => {
+
+        elem.classList.remove(className);
+
+    });
+
+};
+
+//return exect parent node of the element
+const findParent = (elem, parentClass) => {
+
+    let currentNode = elem;
+
+    while (!currentNode.classList.contains(parentClass)) {
+        currentNode = currentNode.parentNode;
+    }
+
+    return currentNode;
+
+};
+
+//get active button step number
+const getActiveStep = elem => {
+    return Array.from(DOMstrings.stepsBtns).indexOf(elem);
+};
+
+//set all steps before clicked (and clicked too) to active
+const setActiveStep = activeStepNum => {
+
+    //remove active state from all the state
+    removeClasses(DOMstrings.stepsBtns, 'js-active');
+
+    //set picked items to active
+    DOMstrings.stepsBtns.forEach((elem, index) => {
+
+        if (index <= activeStepNum) {
+            elem.classList.add('js-active');
+        }
+
+    });
+};
+
+//get active panel
+const getActivePanel = () => {
+
+    let activePanel;
+
+    DOMstrings.stepFormPanels.forEach(elem => {
+
+        if (elem.classList.contains('js-active')) {
+
+            activePanel = elem;
+
+        }
+
+    });
+
+    return activePanel;
+
+};
+
+//open active panel (and close unactive panels)
+const setActivePanel = activePanelNum => {
+
+    const animation = $(DOMstrings.stepFormPanels, 'js-active').attr("data-animation");
+
+    //remove active class from all the panels
+    removeClasses(DOMstrings.stepFormPanels, 'js-active');
+    removeClasses(DOMstrings.stepFormPanels, animation);
+    // removeClasses(DOMstrings.stepFormPanels, 'js-active', 'animate__animated', animation);
+    removeClasses(DOMstrings.stepFormPanels, 'animate__animated');
+
+    //show active panel
+    DOMstrings.stepFormPanels.forEach((elem, index) => {
+        if (index === activePanelNum) {
+
+            elem.classList.add('js-active');
+            // stepFormPanels
+            elem.classList.add('animate__animated', animation);
+
+            setTimeout(function() {
+                removeClasses(DOMstrings.stepFormPanels, 'animate__animated', animation);
+            }, 1200);
+
+            setFormHeight(elem);
+
+        }
+    });
+
+};
+
+
+//set form height equal to current panel height
+const formHeight = activePanel => {
+
+    const activePanelHeight = activePanel.offsetHeight;
+
+    DOMstrings.stepsForm.style.height = `${activePanelHeight}px`;
+
+};
+
+const setFormHeight = () => {
+    const activePanel = getActivePanel();
+
+    formHeight(activePanel);
+};
+
+//STEPS BAR CLICK FUNCTION
+DOMstrings.stepsBar.addEventListener('click', e => {
+
+    //check if click target is a step button
+    const eventTarget = e.target;
+
+    if (!eventTarget.classList.contains(`${DOMstrings.stepsBtnClass}`)) {
+        return;
+    }
+
+    //get active button step number
+    const activeStep = getActiveStep(eventTarget);
+
+    //set all steps before clicked (and clicked too) to active
+    setActiveStep(activeStep);
+
+    //open active panel
+    setActivePanel(activeStep);
+});
+
+//PREV/NEXT BTNS CLICK
+DOMstrings.stepsForm.addEventListener('click', e => {
+
+    const eventTarget = e.target;
+
+    //check if we clicked on `PREV` or NEXT` buttons
+    if (!(eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`) || eventTarget.classList.contains(`${DOMstrings.stepNextBtnClass}`))) {
+        return;
+    }
+
+    //find active panel
+    const activePanel = findParent(eventTarget, `${DOMstrings.stepFormPanelClass}`);
+
+    let activePanelNum = Array.from(DOMstrings.stepFormPanels).indexOf(activePanel);
+
+    //set active step and active panel onclick
+    if (eventTarget.classList.contains(`${DOMstrings.stepPrevBtnClass}`)) {
+        activePanelNum--;
+
+    } else {
+
+        activePanelNum++;
+
+    }
+
+    setActiveStep(activePanelNum);
+    setActivePanel(activePanelNum);
+
+});
+
+//SETTING PROPER FORM HEIGHT ONLOAD
+window.addEventListener('load', setFormHeight, true);
+
+//SETTING PROPER FORM HEIGHT ONRESIZE
+window.addEventListener('resize', setFormHeight, true);
