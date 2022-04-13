3976b775d00e68e3792f5daab610b873421f883d
sibeux <wahabinasrul@gmail.com>
2022-04-13 23:42:17 +0700

    k

---
+++ Main Program/JAVA_OOP/src/com/javaoop/java_dasar/inheritance/Main.java
@@ -0,0 +1,9 @@
+package com.javaoop.java_dasar.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Kucing cat = new Kucing("mario");
+        cat.eat();
+        
+    }
+}
---
+++ Main Program/JAVA_OOP/src/com/javaoop/kelas_terbuka/inheritance/HeroStrength.java
@@ -0,0 +1,6 @@
+package com.javaoop.kelas_terbuka.inheritance;
+
+// subclass, child class, derived clas
+public class HeroStrength extends Hero {
+    // hampa
+}
