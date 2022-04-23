4f0d828093b5c1567ee454f560f8f7bae0de5469
sibeux <wahabinasrul@gmail.com>
2022-04-23 23:02:06 +0700

    detail page

--- Main Program/JAVA_OOP/src/com/javaoop/materi/maven/demo/src/test/java/com/maven/javaoop/DemoMaven.java
+++
@@ -1,6 +0,0 @@
-package com.javaoop.materi.maven.demo.src.test.java.com.maven.javaoop;
-public class DemoMaven {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ Main Program/Web Programming/AJAX/college/detail-page/data.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once "./db.php";
+
+$typing = $_GET["typing"];
+
+$sql = "SELECT * FROM film
+            WHERE 
+        title LIKE '%{$typing}%'
+";
+
+$result = $db->query($sql);
+
+while ($row =  $result->fetch_assoc()) {
+    $genre = explode(" ", $row['rating']);
+    $title = ucwords($row['title']);
+    echo "
+    <div class='col col-lg-6 col-xl-4'>
+                        <div class='product-container'>
+                            <div class='product-card'>
+                                <div class='product-image'>
+                                    <img src='img/film.png' alt='' />
+                                </div>
+                                <div class='product-body'>
+                                    <h3 class='product-title'>{$title}</h3>
+                                    <div class='product-price'>{$row['description']}
+                                    </div>";
+                                    echo "<div class='product-discount'>";
+                                    foreach ($genre as $value) {
+                                        echo "<span class='{$value}'>{$value}</span>" ;
+                                    }
+                                    echo "<br><br>" ;
+                                    echo "<span class='cost'>COST : {$row['replacement_cost']}</span>";
+                                    echo "</div>
+                                    <div class='btn'>
+                                        <i class='bx bxs-edit-alt'></i>
+                                        <i class='bx bxs-trash'></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    ";
+}
