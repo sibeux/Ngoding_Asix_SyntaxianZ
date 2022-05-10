6ab7bde6f8f97325bbd9ac5d9895a1cc75e96108
sibeux <wahabinasrul@gmail.com>
2022-05-10 16:05:44 +0700

    deadliner

--- Main Program/Python/Competitive Programming/TLX/PasarRakyat.py
+++ Main Program/Python/Competitive Programming/TLX/PasarRakyat.py
@@ -1,2 +1,15 @@
-import numpy as np
+n = int(input())
 
+result = 0
+for x in range(1, n + 1):
+    if x == 1:
+        numb = int(input())
+        result = numb
+    else:
+        numb = int(input())
+        if numb % result == 0:
+            result = numb
+        else:
+            result = numb * result
+
+print(result)
