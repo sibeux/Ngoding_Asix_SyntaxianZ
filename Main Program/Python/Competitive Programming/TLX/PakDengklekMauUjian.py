d51b2551156d03e2a9bd3fc2b5a15295f4030624
sibeux <wahabinasrul@gmail.com>
2022-05-10 13:11:11 +0700

    tlx competitive programming

---
+++ Main Program/Python/Competitive Programming/TLX/PakDengklekMauUjian.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if n > 100 or n < 0:
+    print("TIDAK")
+else:
+    print("YA")
---
+++ Main Program/Python/Competitive Programming/TLX/PakDengklekdanOperasiSederhana.py
@@ -0,0 +1,9 @@
+n = str(input())
+
+numb = n.split()
+
+print(int(numb[0]) + int(numb[1]))
+print(int(numb[0]) - int(numb[1]))
+print(int(numb[0]) * int(numb[1]))
+print(int(numb[0]) // int(numb[1]))
+print(int(numb[0]) % int(numb[1]))
