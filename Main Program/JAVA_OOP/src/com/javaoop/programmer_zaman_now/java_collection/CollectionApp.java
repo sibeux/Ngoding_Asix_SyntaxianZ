063622acf5d051337f2bb23c39cdebddf31261f2
sibeux <wahabinasrul@gmail.com>
2022-05-11 19:34:28 +0700

    java collection

---
+++ Main Program/JAVA_OOP/src/com/javaoop/programmer_zaman_now/java_collection/CollectionApp.java
@@ -0,0 +1,33 @@
+package com.javaoop.programmer_zaman_now.java_collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionApp {
+    public static void main(String[] args) {
+        
+        Collection<String> names = new ArrayList<>();
+
+        names.add("sibe");
+        names.add("habqi");
+        names.add("habi");
+        names.addAll(List.of("nasrul","biqi","habi"));
+
+        names.remove("habqi");
+        names.remove("budi"); // no impact karena return boolean
+        names.removeAll(List.of("sibe","nasrul"));
+
+        for (var value : names){
+            System.out.println(value);
+        }
+
+        System.out.println(
+            names.contains("habibi")
+        );
+
+        System.out.println(
+            names.containsAll(List.of("biqi","habi"))
+        );
+    }
+}
