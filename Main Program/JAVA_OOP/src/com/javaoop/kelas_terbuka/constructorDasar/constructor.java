2d81c54f91ccffb3f2be9832a38962b1d760118b
sibeux <wahabinasrul@gmail.com>
2022-07-03 21:43:13 +0700

    basic js

--- Main Program/JAVA_OOP/src/com/javaoop/kelas_terbuka/constructorDasar/constructor.java
+++ Main Program/JAVA_OOP/src/com/javaoop/kelas_terbuka/constructorDasar/constructor.java
@@ -1,24 +1,24 @@
 package com.javaoop.kelas_terbuka.constructorDasar;
 
 // class tanpa constructor / class polos
-class polos{
+class polos {
     String dataString;
     int dataInteger;
 }
 
 // class dengan constructor
-class mahasiswaa{
+class mahasiswaa {
     String nama;
     String NIM;
     String jurusan;
 
     // constructor dipanggil saat object pertama kali dibuat
     // Mahasiswa(){
-    //     System.out.println("ini adalah constructor");
+    // System.out.println("ini adalah constructor");
     // }
 
     // constructor dengan parameter
-    mahasiswaa(String inputNama, String inputNIM, String inputJurusan){
+    mahasiswaa(String inputNama, String inputNIM, String inputJurusan) {
         nama = inputNama;
         NIM = inputNIM;
         jurusan = inputJurusan;
@@ -31,16 +31,16 @@ class mahasiswaa{
 
 public class constructor {
     public static void main(String[] args) throws Exception {
-     
-        mahasiswaa mahasiswaa1 = new mahasiswaa("sibe","61731624","Teknik Nuklir");
-        mahasiswaa mahasiswaa2 = new mahasiswaa("habi","62131274","Teknik elektro");
+
+        mahasiswaa mahasiswaa1 = new mahasiswaa("sibe", "61731624", "Teknik Nuklir");
+        mahasiswaa mahasiswaa2 = new mahasiswaa("habi", "62131274", "Teknik elektro");
         System.out.println("mahasiswaa1.nama : " + mahasiswaa1.nama);
         System.out.println("mahasiswaa2.jurusan : " + mahasiswaa2.jurusan);
         // polos objectPolos = new Polos();
         // objectPolos.dataString = "Polos";
         // objectPolos.dataInteger = 0;
 
-        //System.out.printIn(objectPolos.dataString);
-        //System.out.printIn(objectPolos.dataInteger);
+        // System.out.printIn(objectPolos.dataString);
+        // System.out.printIn(objectPolos.dataInteger);
     }
 }
---
+++ Main Program/JS/basic-js/typeof.js
@@ -0,0 +1,4 @@
+let a = 1.3;
+let b = "1.3";
+console.log(typeof a === "number");
+console.log(typeof b === "number");
