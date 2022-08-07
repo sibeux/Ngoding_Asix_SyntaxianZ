affc64a385b890921196eaf43b07f92ddab382fc
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-07 21:20:25 +0700

    java gui text field

--- Main_Program/JAVA_OOP/src/com/java_gui/Container.java
+++ Main_Program/JAVA_OOP/src/com/java_gui/Container.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Container {
@@ -13,7 +14,7 @@ public class Container {
         JFrame frame = new JFrame("This is title"); // membuat frame baru
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // mengatur default close operation
 
-        frame.setResizable(false); // mengatur frame tidak dapat di-resize
+        frame.setResizable(true); // mengatur frame dapat di-resize
         frame.setSize(420, 420); // mengatur ukuran frame
         frame.setLocationRelativeTo(null); // mengatur frame berada di tengah layar
         frame.setLayout(null);
@@ -31,8 +32,25 @@ public class Container {
 
         // bagian component ==========================
         JLabel judulLabel = new JLabel("Ini Adalah Aplikasi RankerIN", SwingConstants.CENTER); // membuat label baru
-        judulLabel.setBounds(100, 30,210, 50); // mengatur ukuran label
+        judulLabel.setBounds(100, 30,210, 30); // mengatur ukuran label
         frame.add(judulLabel); // menambahkan label ke frame
         // ========================================
+
+        // menambahkan field ke frame ===============
+        JLabel textKodeBuku = new JLabel("Kode Buku");
+        textKodeBuku.setBounds(20, 80, 150, 30);
+        frame.add(textKodeBuku);
+
+        JTextField fieldKodeBuku = new JTextField(); // membuat text field baru
+        fieldKodeBuku.setBounds(150, 80, 150, 30);
+        frame.add(fieldKodeBuku); // menambahkan text field ke frame
+
+        JLabel textNamaBuku = new JLabel("Nama Buku");
+        textNamaBuku.setBounds(20, 120, 150, 30);
+        frame.add(textNamaBuku);
+
+        JTextField fieldNamaBuku = new JTextField(); // membuat text field baru
+        fieldNamaBuku.setBounds(150, 120, 150, 30);
+        frame.add(fieldNamaBuku); // menambahkan text field ke frame
     }
 }
