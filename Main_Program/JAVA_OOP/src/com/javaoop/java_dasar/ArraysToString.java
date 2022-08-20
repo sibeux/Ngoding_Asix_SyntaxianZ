9ad664174f7f859cd52dfaf58ea064adc5f5a3c6
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-20 16:53:13 +0700

    arrays to string

---
+++ Main_Program/JAVA_OOP/src/com/javaoop/java_dasar/ArraysToString.java
@@ -0,0 +1,13 @@
+package com.javaoop.java_dasar;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args) {
+        String sayuran[] = {"kentang", "tomat", "wortel"};
+        String buahan[][] = {{"apel","mangga"},{"sirsak","semangka"}};
+
+        System.out.println(Arrays.toString(sayuran));
+        System.out.println(Arrays.deepToString(buahan));
+    }
+}
