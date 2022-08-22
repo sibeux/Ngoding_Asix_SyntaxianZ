a4bc221d73d31cd63863773c57fd1238134bf165
Nasrul Wahabi <wahabinasrul@gmail.com>
2022-08-23 04:14:03 +0700

    unit test

--- Main_Program/JAVA_OOP/src/test/UnitTest.java
+++ Main_Program/JAVA_OOP/src/test/UnitTest.java
@@ -1,8 +1,11 @@
 package test;
 
 public class UnitTest {
+    public static int sum() {
+        return 6-1;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(sum());
     }
     
 }
