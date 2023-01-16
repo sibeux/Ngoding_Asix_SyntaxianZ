2280905825d6aa846bc8c2930bf16b89ff80fcca
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-01-17 00:41:17 +0700

    dart basic

---
+++ Main_Program/Dart/dart_basic/class.dart
@@ -0,0 +1,12 @@
+class Person {
+  String name = "sibe";
+  int age = 20;
+}
+
+void main(List<String> args) {
+  Person person1 = Person();
+  var person2 = Person();
+
+  print(person1.name);
+  print(person2.age);
+}
