ba0247573196cc81a720ebf01b776bafd149e082
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-02-24 22:13:44 +0700

    oamaa

---
+++ Main_Program/Python/Program Random/DeleteEnterString.py
@@ -0,0 +1,14 @@
+text = """ implementasiintegrasi
+sistem SIHALAL
+dengan sistem
+Lembaga Pemeriksa
+Halal (LPH)
+ Memodifikasi API untuk
+integrasi sistem dengan
+LNSW
+ Membuat API untuk
+integrasi sistem dengan
+marketplace/instansi/lem
+bagaterkait
+"""
+print(text.replace("\n", " "))
