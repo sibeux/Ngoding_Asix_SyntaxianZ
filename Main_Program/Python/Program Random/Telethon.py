f8d127e3bc93f0312746cebe718ac5d42eff80d6
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-29 23:01:47 +0700

    okejek

---
+++ Main_Program/Python/Program Random/Telethon.py
@@ -0,0 +1,11 @@
+from telethon.sync import TelegramClient, events
+
+with TelegramClient('name', api_id, api_hash) as client:
+   client.send_message('me', 'Hello, myself!')
+   print(client.download_profile_photo('me'))
+
+   @client.on(events.NewMessage(pattern='(?i).*Hello'))
+   async def handler(event):
+      await event.reply('Hey!')
+
+   client.run_until_disconnected()
