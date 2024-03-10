62b096cc8a15573ec89cb4ce337449874172de26
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-03-11 03:38:33 +0700

    okejek

---
+++ Main_Program/Python/Program Random/BackupCodeWahabinasrul.py
@@ -0,0 +1,17 @@
+print("SIMPAN KODE CADANGAN")
+print("Simpan kode cadangan ini di tempat yang aman, tetapi dapat diakses.")
+
+"""
+
+1. 9335 3155		 6. 5192 5961
+2. 6472 1677		 7. 6051 1949
+3. 1396 2353		 8. 6453 8054
+4. 2151 2406		 9. 0665 8823
+5. 3432 8040		10. 3516 7957
+
+"""
+print("(wahabinasrul@gmail.com)")
+
+print("* Anda hanya dapat menggunakan tiap kode cadangan satu kali.")
+print("* Perlu yang lainnya? Kunjungi https://g.co/2sv")
+print("* Kode ini dibuat tanggal: 10 Mar 2024.")
