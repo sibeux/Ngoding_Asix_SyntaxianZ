5ed4be4515a4e0480082876dcd9a0b3fb02dfe93
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-03-24 19:59:05 +0700

    Update music progress bar and input style

---
+++ Main_Program/Web Programming/AJAX/test/music-player-codingNepal/js/music-list.js
@@ -0,0 +1,33 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+	{
+		name: "Harley Bird - Home",
+		artist: "Jordan Schor",
+		img: "https://i.scdn.co/image/ab67616d0000b2733492c68445c4135f4aa27105",
+		src: "https://github.com/sibeux/license-sibeux/raw/MyProgram/%E6%97%A5%E6%9C%AC%E3%81%AE%E6%AD%8C/BURNOUT%20SYNDROMES-%20PHOENIX/BURNOUT%20SYNDROMES-%20PHOENIX.mp3",
+	},
+	{
+		name: "Ikson Anywhere – Ikson",
+		artist: "Audio Library",
+		img: "https://i.scdn.co/image/ab67616d0000b2733492c68445c4135f4aa27105",
+		src: "https://github.com/sibeux/license-sibeux/raw/MyProgram/%E6%97%A5%E6%9C%AC%E3%81%AE%E6%AD%8C/BURNOUT%20SYNDROMES-%20PHOENIX/BURNOUT%20SYNDROMES-%20PHOENIX.mp3",
+	},
+
+	// like this paste it and remember to give comma after ending of this bracket }
+	// {
+	//   name: "Here is the music name",
+	//   artist: "Here is the artist name",
+	//   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+	//   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+	// }
+];
